317c0ef005bb8c75cdef5ee99666b57089ae7e6e
amemme-alt <amemme@gmail.com>
2026-08-29 01:40:01 +0200

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'jp8-calc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.JPG'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
